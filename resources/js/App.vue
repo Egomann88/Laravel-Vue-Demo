a56865ad560f338c590f4150a6269968466fe5ab
egomann88 <justin-urbanek@bluewin.ch>
2023-08-09 13:25:44 +0200

    feat: install vueJS

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const msg = ref('Hello, Vuejs with Laravel');
+
+console.log(msg.value);
+</script>
+
+<template>
+    <h1> Hello, Vuejs with Laravel </h1>
+</template>
