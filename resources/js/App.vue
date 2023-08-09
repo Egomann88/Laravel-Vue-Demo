d6ed31ad3119e6bfb3d66fef454f6d17c93a108d
egomann88 <justin-urbanek@bluewin.ch>
2023-08-09 14:13:29 +0200

    feat: vue-router

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import { RouterView } from 'vue-router';
 
 const msg = ref('Hello, Vuejs with Laravel');
 
@@ -8,4 +9,5 @@ console.log(msg.value);
 
 <template>
     <h1> Hello, Vuejs with Laravel </h1>
+    <RouterView></RouterView>
 </template>
