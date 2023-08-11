14bd100093306f92590a7ad36600379d917226a6
egomann88 <justin-urbanek@bluewin.ch>
2023-08-11 10:40:41 +0200

    feat: UserCards migration

---
+++ database/migrations/2023_08_11_061512_create__userCards_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('userCards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->references('id')->on('users')->constrained()->onDelete('cascade');
+            $table->foreignId('card_id')->nullable()->references('id')->on('cards')->constrained()->onDelete('cascade');
+            $table->integer('quantity')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('userCards');
+    }
+};
