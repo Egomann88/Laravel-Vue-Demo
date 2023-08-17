32f92c5a2470b05fa32230f8039a4d13d1189c9f
egomann88 <justin-urbanek@bluewin.ch>
2023-08-17 19:08:42 +0200

    feat: banner finished

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -10,37 +10,61 @@
         <!-- <img src="./banner.jpg" class="absolute opacity-50 w-[100vw] h-[100vh]" /> -->
         <div class="bgImgCluster min-h-screen px-8">
             <!-- top line -->
-            <div class="grid grid-cols-12 h-16">
+            <div class="grid grid-cols-12 h-16 my-8">
                 <div class="col-span-3 my-auto flex">
-                    <img src="./logo.png" class="w-16 h-16" />
-                    <h2 class="text-2xl mx-4 my-auto">Gacha</h2>
+                    <img src="../assets/wishingStar.svg" class="w-16 h-16" />
+                    <h2 class="text-2xl font-semibold text-white mx-4 my-auto">Gacha</h2>
                 </div>
-                <div class="col-span-6 mx-8 my-auto flex justify-between">
-                    <button class="mx-2 h-16 w-40 rounded-lg py-2">
-                        <img src="./logo.png" class="w-32 h-12 mx-auto" />
+                <div class="hidden lg:flex col-span-6 mx-8 my-auto justify-between">
+                    <button
+                        class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                        <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
                     </button>
-                    <button class="mx-2 h-16 w-40 rounded-lg py-2">
-                        <img src="./logo.png" class="w-32 h-12 mx-auto" />
+                    <button
+                        class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                        <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
                     </button>
-                    <button class="mx-2 h-16 w-40 rounded-lg py-2">
-                        <img src="./logo.png" class="w-32 h-12 mx-auto" />
+                    <button
+                        class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                        <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
                     </button>
-                    <button class="mx-2 h-16 w-40 rounded-lg py-2">
-                        <img src="./logo.png" class="w-32 h-12 mx-auto" />
+                    <button
+                        class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                        <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
                     </button>
                 </div>
-                <div class="col-span-3 px-8 my-auto">
+                <div class="col-span-9 lg:col-span-3 px-8 my-auto ml-auto">
                     <div class="w-fit bg-gray-600 bg-opacity-60 text-white rounded-full flex pr-6 py-2">
-                        <img src="./logo.png" class="w-8 h-8 ml-4" />
+                        <img src="https://placehold.co/32x32" class="w-8 h-8 ml-4" />
                         <h1 class="text-xl mx-4"><b>131312</b></h1>
                     </div>
                 </div>
             </div>
+            <!-- banner's mobile -->
+            <div class="flex lg:hidden m-8 justify-between">
+                <button
+                    class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                    <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
+                </button>
+                <button
+                    class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                    <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
+                </button>
+                <button
+                    class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                    <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
+                </button>
+                <button
+                    class="bg-violet-700 bg-opacity-60 mx-2 h-16 w-40 rounded-lg py-2 border-black hover:border-2 transform-gpu duration-100">
+                    <img src="https://placehold.co/128x48" class="w-32 h-12 mx-auto " />
+                </button>
+            </div>
+
             <!-- banner -->
             <div class="">
                 <!-- w-[1280px] h-[580px] -->
                 <!-- full-length grid -->
-                <div class="w-4/5 mx-auto">
+                <div class="w-full lg:w-4/5 mx-auto">
                     <!-- padding in grid -->
                     <div class="bgImgBanner bg-gray-200 rounded-xl">
                         <div class="grid grid-cols-6 h-full px-14">
@@ -89,7 +113,7 @@
                                     -- Hier kommt die Karte hin --
                                 </div>
                                 <!-- name and description -->
-                                <div class="bg-violet-800 bg-opacity-75 relative my-11 px-4 w-[400px] h-16">
+                                <div class="bg-violet-800 bg-opacity-75 relative my-11 px-4 w-[380px] h-16">
                                     <div class="w-[350px] absolute -mt-8">
                                         <h1 class="text-3xl">Name der Karte dfde daa da</h1>
                                     </div>
@@ -116,14 +140,11 @@
                     </button>
                 </div>
                 <div class="col-span-5 flex justify-end">
-                    <button
-                        class="mx-4 px-12 py-4 bg-neutral-100 border-2 border-yellow-500 rounded-full text-xl font-semibold text-yellow-700 transform-gpu duration-300 hover:bg-neutral-300">1x
+                    <button class="pullBtn">1x
                         ziehen</button>
-                    <button
-                        class="mx-4 px-12 py-4 bg-neutral-100 border-2 border-yellow-500 rounded-full text-xl font-semibold text-yellow-700 transform-gpu duration-300 hover:bg-neutral-300">10x
+                    <button class="pullBtn">10x
                         ziehen</button>
-                    <button
-                        class="mx-4 px-12 py-4 bg-neutral-100 border-2 border-yellow-500 rounded-full text-xl font-semibold text-yellow-700 transform-gpu duration-300 hover:bg-neutral-300">Bis
+                    <button class="pullBtn">Bis
                         5-Sterne ziehen</button>
                 </div>
             </div>
@@ -144,5 +165,9 @@
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
+}
+
+.pullBtn {
+    @apply mx-4 px-12 py-4 bg-neutral-100 border-2 border-yellow-500 rounded-full text-xl font-semibold text-yellow-700 transform-gpu duration-300 active:bg-orange-100;
 }
 </style>
